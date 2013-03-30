a67c6434940e4ef0ead8ec23ac95c1fd045f4ebb
schamberlain <myrmecocystus@gmail.com>
2013-03-29 23:22:54 -0700

    edits

---
+++ rgbif_params.r
@@ -0,0 +1,56 @@
+scientificname = NULL
+taxonconceptKey = NULL
+
+dataproviderkey = NULL
+dataresourcekey = NULL
+institutioncode = NULL
+
+collectioncode = NULL
+catalognumber = NULL
+resourcenetworkkey = NULL
+
+basisofrecordcode = NULL
+minlatitude = NULL
+maxlatitude = NULL
+
+minlongitude = NULL
+maxlongitude = NULL
+minaltitude = NULL
+maxaltitude = NULL
+
+mindepth = NULL
+maxdepth = NULL
+cellid = NULL
+centicellid = NULL
+
+typesonly = NULL
+georeferencedonly = NULL
+coordinatestatus = NULL
+
+coordinateissues = NULL
+hostisocountrycode = NULL
+originisocountrycode = NULL
+
+originregioncode = NULL
+startdate = NULL
+enddate = NULL
+startyear = NULL
+
+endyear = NULL
+year = NULL
+month = NULL
+day = NULL
+modifiedsince = NULL
+
+startindex = NULL
+maxresults = 10
+format = NA
+icon = NULL
+
+mode = NULL
+stylesheet = NULL
+removeZeros = FALSE
+writecsv = NULL
+
+url = "http://data.gbif.org/ws/rest/occurrence/list"
+curl = getCurlHandle()
